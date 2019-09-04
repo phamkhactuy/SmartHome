849cedb47c3a58e0bf95486e2ba0c5cb53ff72f1
Tien Truong <tientruong@coloralpha.com>
2019-09-04 23:48:29 +0700

    Socket server: comment code

--- GroupLang/Server/socketServer-windows/index.js
+++ GroupLang/Server/socketServer-windows/index.js
@@ -1,31 +1,30 @@
-const PORT = 3484;									//Đặt địa chỉ Port được mở ra để tạo ra chương trình mạng Socket Server
+const PORT = 3484;									
 
 var http = require('http');
-var express = require('express');							//#include thư viện express - dùng để tạo server http nhanh hơn thư viện http cũ
-var socketio = require('socket.io')				//#include thư viện socketio
+var express = require('express');						
+var socketio = require('socket.io')				
 
 //var ip = require('ip');
-var app = express();									//#Khởi tạo một chương trình mạng (app)
+var app = express();									
 var server = http.Server(app)
 
-var io = socketio(server);								//#Phải khởi tạo io sau khi tạo app
+var io = socketio(server);							
+//Tạo namespace để phân biêt SocketClient trên Esp và webapp
+var webapp_nsp = io.of('/webapp')			
+var esp8266_nsp = io.of('/esp8266')				
 
-var webapp_nsp = io.of('/webapp')				//namespace của webapp
-var esp8266_nsp = io.of('/esp8266')				//namespace của esp8266
-
-var middleware = require('socketio-wildcard')();		//Để có thể bắt toàn bộ lệnh!
-esp8266_nsp.use(middleware);									//Khi esp8266 emit bất kỳ lệnh gì lên thì sẽ bị bắt
-webapp_nsp.use(middleware);									//Khi webapp emit bất kỳ lệnh gì lên thì sẽ bị bắt
+var middleware = require('socketio-wildcard')();	//Để có thể bắt toàn bộ lệnh!
+esp8266_nsp.use(middleware);						//Khi esp8266 emit bất kỳ lệnh gì lên thì sẽ bị bắt
+webapp_nsp.use(middleware);							//Khi webapp emit bất kỳ lệnh gì lên thì sẽ bị bắt
 
 server.listen(PORT);										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
 
 //Cài đặt webapp các fie dữ liệu tĩnh
-app.use(express.static("node_modules/mobile-angular-ui")) 			// Có thể truy cập các file trong node_modules/mobile-angular-ui từ xa
-app.use(express.static("node_modules/angular")) 							// Có thể truy cập các file trong node_modules/angular từ xa
-app.use(express.static("node_modules/angular-route")) 				// Có thể truy cập các file trong node_modules/angular-route từ xa
-app.use(express.static("node_modules/socket.io-client")) 				// Có thể truy cập các file trong node_modules/socket.io-client từ xa
-app.use(express.static("node_modules/angular-socket-io"))			// Có thể truy cập các file trong node_modules/angular-socket-io từ xa
-app.use(express.static("webapp")) 													// Dùng để lưu trữ webapp
+app.use(express.static("node_modules/mobile-angular-ui")) 		
+app.use(express.static("node_modules/angular")) 							
+app.use(express.static("node_modules/angular-route")) 				
+app.use(express.static("node_modules/socket.io-client")) 
+app.use(express.static("node_modules/angular-socket-io"))			
 
 
 //giải nén chuỗi JSON thành các OBJECT
@@ -38,7 +37,7 @@ function ParseJson(jsondata) {
 }
 
 
-//Bắt các sự kiện khi esp8266 kết nối
+//Bắt các sự kiện từ esp8266 gửi lên -> gửi toàn bộ dữ liệu cho webapp
 esp8266_nsp.on('connection', function(socket) {
 	console.log('esp8266 connected')
 	
@@ -46,10 +45,9 @@ esp8266_nsp.on('connection', function(socket) {
 		console.log("Disconnect socket esp8266")
 	})
 	
-	
-	//nhận được bất cứ lệnh nào
+
 	socket.on("*", function(packet) {
-		console.log("esp8266 rev and send to webapp packet: ", packet.data) //in ra để debug
+		console.log("esp8266 rev and send to webapp packet: ", packet.data)
 
 		var eventName = packet.data[0]
 		var eventJson = packet.data[1] || {} //nếu gửi thêm json thì lấy json từ lệnh gửi, không thì gửi chuỗi json rỗng, {}
@@ -58,13 +56,13 @@ esp8266_nsp.on('connection', function(socket) {
 	})
 })
 
-//Bắt các sự kiện khi webapp kết nối
+//Bắt các sự kiện từ webapp -> gửi toàn bộ dữ liệu xuống esp8266
 
 webapp_nsp.on('connection', function(socket) {
 	
 	console.log('webapp connected')
 	
-	//Khi webapp socket bị mất kết nối
+
 	socket.on('disconnect', function() {
 		console.log("Disconnect socket webapp")
 	})
